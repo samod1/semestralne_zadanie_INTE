cdc4060a001cb8119fced75fc7d53780f86e5f31
samod <samuel.domin@dominanz.sk>
2021-08-27 12:46:15 +0200

    fix

--- ciselnik.php
+++ ciselnik.php
@@ -2,7 +2,11 @@
 $conn = "";
 include "config.php";
 
-$query = "SELECT názov, ulica, PSČ FROM Fakulty WHERE `kód školy`= 702000000";  //uspodiadaj ASC od najmensieho po najvacsi
+//TODO pridat tituly pred menom, za menom
+
+$query = "SELECT meno, priezvisko, F.názov, KO.názov FROM tbl_student_new 
+    INNER JOIN Fakulty F on tbl_student_new.fakulta = F.kód
+    INNER JOIN Krajny_OSN KO on tbl_student_new.krajina = KO.kód";
 $result = mysqli_query($conn, $query); // mysqli_query - vykona prikaz
 $pocetRiadkov = mysqli_num_rows($result);
 if (!$result) {
@@ -18,9 +22,12 @@ if ($pocetRiadkov == 0) {
 
 <table>
     <tr>
-        <th>Názov</th>
-        <th>Adresa</th>
-        <th>PSČ</th>
+        <th>titul pred menom</th>
+        <th>Meno</th>
+        <th>Priezvisko</th>
+        <th>titul za menom</th>
+        <th>Fakulta</th>
+        <th>Krajina</th>
     </tr>
 
     <?
@@ -30,102 +37,18 @@ if ($pocetRiadkov == 0) {
         ?>
         <tr>
 
+            <td><?php echo $row["titulPredMenom"] ?></td>
+            <td><?php echo $row["meno"] ?></td>
+            <td><?php echo $row["priezvisko"] ?></td>
+            <td><?php echo $row["titulZaMenom"] ?></td>
             <td><?php echo $row["názov"] ?></td>
-            <td><?php echo $row["ulica"] ?></td>
-            <td><?php echo $row["PSČ"] ?></td>
+            <td><?php echo $row["názov"] ?></td>
+
         </tr>
 
         <?php
     }
-
-
     ?>
 </table>
-
-<?php
-$query = "SELECT názov, kód FROM Krajny_OSN";
-$result = mysqli_query($conn, $query);
-$pocetRiadkov = mysqli_num_rows($result);
-if (!$result) {
-    echo "Error: Neda sa vykonat prikaz SQL: " . $query . ".<br>" . PHP_EOL;
-    exit;
-}
-if ($pocetRiadkov == 0) {
-    echo "Nemam co zobrazit";
-}
-?>
-<br>
-<br>
-<label>Krajna</label>
-<select name="country" id="coutries">
-
-
-    <?php
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        ?>
-        <option value="<?php echo $row["kód"] ?>"><?php echo $row["názov"] ?></option>
-
-    <?php } ?>
-</select>
-
-<?php
-$query = "SELECT názov, kód FROM Vysoke_skoly";
-$result = mysqli_query($conn, $query);
-$pocetRiadkov = mysqli_num_rows($result);
-if (!$result) {
-    echo "Error: Neda sa vykonat prikaz SQL: " . $query . ".<br>" . PHP_EOL;
-    exit;
-}
-if ($pocetRiadkov == 0) {
-    echo "Nemam co zobrazit";
-}
-?>
-<br>
-<br>
-<label>Univerzita</label>
-<select name="university" id="university">
-
-
-    <?php
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        ?>
-        <option value="<?php echo $row["kód"] ?>"><?php echo $row["názov"] ?></option>
-
-    <?php } ?>
-</select>
-
-<?php
-$query = "SELECT názov, kód FROM Fakulty";
-$result = mysqli_query($conn, $query);
-$pocetRiadkov = mysqli_num_rows($result);
-if (!$result) {
-    echo "Error: Neda sa vykonat prikaz SQL: " . $query . ".<br>" . PHP_EOL;
-    exit;
-}
-if ($pocetRiadkov == 0) {
-    echo "Nemam co zobrazit";
-}
-?>
-<br>
-<br>
-<label>Fakulta</label>
-<select name="university" id="university">
-
-
-    <?php
-
-    while ($row = mysqli_fetch_assoc($result)) {
-        ?>
-        <option value="<?php echo $row["kód"] ?>"><?php echo $row["názov"] ?></option>
-
-        <?php
-    }
-    mysqli_close($conn);
-    ?>
-</select>
-
-
 </body>
 </html>
